1bbe5629acdbaac413d1a9b562eebbf5271f0b16
nwnp <jjw_0418@naver.com>
2022-07-06 15:16:07 +0900

    feat: api test

--- frontend/src/components/views/LandingPage/LandingPage.js
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function LandingPage() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/test")
+      .then((res) => console.log(res.data));
+  });
   return <div>LandingPage</div>;
 }
 
