1dadf7371a1f310308abeb2db08b52ead58f226b
nwnp <jjw_0418@naver.com>
2022-07-06 11:16:28 +0900

    frontend: 구조 생성

---
+++ frontend/src/components/views/NavBar/NavBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function NavBar() {
+  return <div>NavBar</div>;
+}
+export default NavBar;
---
+++ frontend/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function RegisterPage() {
+  return <div>RegisterPage</div>;
+}
+export default RegisterPage;
