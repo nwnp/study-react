cc998f4434f1010649237d2372145f953d0afc1f
nwnp <jjw_0418@naver.com>
2022-07-07 23:49:43 +0900

    change the file paht

--- backend/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://nwnp:mongo@cluster0.67wuqkd.mongodb.net/test",
-};
--- frontend/src/components/views/RegisterPage/RegisterPage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function RegisterPage() {
-  return <div>RegisterPage</div>;
-}
-export default RegisterPage;
